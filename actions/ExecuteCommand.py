2ded9361b8be824dda0afeec849372d12a3db8e0
root <wtdniels@hotmail.com>
2017-05-03 15:03:38 +0200

    added exceptions

--- actions/ExecuteCommand.py
+++ actions/ExecuteCommand.py
@@ -9,23 +9,26 @@ class selfheal(Action):
 	StackstormPath = "/opt/stackstorm/packs/fenego/" 
 
 	if company is not "null":
-		with open(StackstormPath +"SSH/"+ company + '_SSH') as ReadFile: 
-			for line in ReadFile:				  #Reads the file and loads the variables
-				if "host:\'" + host in line:
-				    hostname = " "
-				    Credentials = line.split("; ")
-				    Credentials[0] = Credentials[0].strip("host:")
-				    Host = Credentials[0].strip("\'")
-				    Credentials[1] = Credentials[1].strip("username:")
-				    Username = Credentials[1].strip("\'")
-				    Credentials[2] = Credentials[2].strip("pem-file:")
-				    Credentials[2] = Credentials[2].strip("\n")
-				    Pempath = Credentials[2].strip("\'")
-				    #Creating an SSH connection with pem key and running an python script on the remote host
-				    print "trying to execute cmd " + cmd
-				    returnvalue = os.system("ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'"+ cmd +"\'")
-				    if returnvalue != 0 :
-					return (False,"Error executing command on remote host")
+		try:
+			with open(StackstormPath +"SSH/"+ company + '_SSH') as ReadFile: 
+				for line in ReadFile:				  #Reads the file and loads the variables
+					if "host:\'" + host in line:
+					    hostname = " "
+					    Credentials = line.split("; ")
+					    Credentials[0] = Credentials[0].strip("host:")
+					    Host = Credentials[0].strip("\'")
+					    Credentials[1] = Credentials[1].strip("username:")
+					    Username = Credentials[1].strip("\'")
+					    Credentials[2] = Credentials[2].strip("pem-file:")
+					    Credentials[2] = Credentials[2].strip("\n")
+					    Pempath = Credentials[2].strip("\'")
+					    #Creating an SSH connection with pem key and running an python script on the remote host
+					    print "trying to execute cmd " + cmd
+					    returnvalue = os.system("ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'"+ cmd +"\'")
+					    if returnvalue != 0 :
+						return (False,"Error executing command on remote host")
+		except:
+			return (False,"Couldn't make ssh connection") 
 	
 		if hostname is "null":
 			  return (False,"hostname wasn't found")   			 
--- actions/logchecker.py
+++ actions/logchecker.py
@@ -5,25 +5,32 @@ from st2actions.runners.pythonrunner import Action
 
 class selfheal(Action):
     def run(self, agg_key , alert_id , alert_metric, alert_query, alert_transition, alert_status, title,last_updated,date,event_type,body,user,link,priority,tags,host,snapshot,frequency , period , company):
-	again = "no"
+	limit = "no"
 	times = 0
 	OneHourAgo = datetime.now() - timedelta(hours=period)
+	try:
+		ReadFile = open("/opt/stackstorm/packs/fenego/logs/" + company + "_logfile.log","r+")
+		lines = ReadFile.readlines()
+		ReadFile.seek(0)
+		ReadFile.write(str(datetime.now()) + "; hostname:"+host + " , " + alert_query +"\n")
+		for line in lines:
+			values = line.split("; ")
+			values[0] = datetime.strptime(values[0], "%Y-%m-%d %H:%M:%S.%f")
+			if values[0] > OneHourAgo:
+				ReadFile.write(line)
+		ReadFile.truncate()
+		ReadFile.close()
+	except:
+		return (False, "Error loading the log file")
+	
+	try:
+		with open("/opt/stackstorm/packs/fenego/logs/" + company + "_logfile.log") as ReadFile:
+			for line in ReadFile:
+				if host in line and alert_query in line:
+					times = times + 1
+	except:
+		return (False, "Error reading the log file.")
 
-	ReadFile = open("/opt/stackstorm/packs/fenego/logs/" + company + "logfile.log","r+")
-	lines = ReadFile.readlines()
-	ReadFile.seek(0)
-	ReadFile.write(str(datetime.now()) + "; hostname:"+host + " , " + alert_query +"\n")
-	for line in lines:
-		values = line.split("; ")
-		values[0] = datetime.strptime(values[0], "%Y-%m-%d %H:%M:%S.%f")
-		if values[0] > OneHourAgo:
-		        ReadFile.write(line)
-	ReadFile.truncate()
-	ReadFile.close()
-	with open("/opt/stackstorm/packs/fenego/logs/" + company + "_logfile.log") as ReadFile:
-		for line in ReadFile:
-		        if host in line and alert_query in line:
-				times = times + 1
 	if times > frequency :
 		            print "Procces failed to manny times within the time limit"
 		            again = "yes"
@@ -48,7 +55,7 @@ class selfheal(Action):
 		            except:
 		                    print("Error:unable to send mail")
 
-	if again == "no":
+	if limit == "no":
         	return True
 	else:
         	return (False, "To manny restart attempts")

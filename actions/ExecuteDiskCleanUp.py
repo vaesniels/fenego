094c9ba46a0d6cd95d91d55d2f8b0f73741ea6db
root <wtdniels@hotmail.com>
2017-05-08 11:29:15 +0200

    some changes

--- actions/ExecuteDiskCleanUp.py
+++ actions/ExecuteDiskCleanUp.py
@@ -7,6 +7,10 @@ class selfheal(Action):
     def run(self, agg_key , alert_id , alert_metric, alert_query, alert_transition, alert_status, title,last_updated,date,event_type,body,user,link,priority,tags,host,snapshot,size,day,path,company,stackstormpath):
         
 	hostname = "null"
+	DiskSpaceToClean = 0.0
+	TotalDiskSpaceUsed= 0.0
+	DiskSpaceAfterClean = 0.0
+
 	try:
 		with open(stackstormpath +"SSH/"+ company + '_SSH') as ReadFile: 
 			for line in ReadFile:				  #Reads the file and loads the variables
@@ -21,6 +25,22 @@ class selfheal(Action):
 					Credentials[2] = Credentials[2].strip("\n")
 					Pempath = Credentials[2].strip("\'")
 						    #Creating an SSH connection with pem key and running an python script on the remote host
+					output = subprocess.check_output("sudo ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'df -h\'", shell=True)
+					output = output.split("\n")
+					for x in range(1, len(output) - 1):
+    						if output[x].startswith("/dev/"):
+        						disk = output[x].split(" ")
+        						if "G" in disk[9] :
+							    diskused = disk[9].strip("G")
+							    diskused = diskused.replace(",", ".")
+        						if "T" in disk[9] :
+							    diskused = disk[9].strip("T")
+							    diskused = diskused.replace(",", ".")
+							    diskused = float(diskused) * 1024
+							SpaceToClean = (float(diskused) / 100 ) * 10
+							TotalDiskSpaceUsed += float(diskused)
+							DiskSpaceToClean = DiskSpaceToClean + SpaceToClean
+
 					returnvalue = os.system("ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host  + " python -u - "+ alert_id + " " + path +" " + size + " " + day + " < " + stackstormpath + "python/DiskCleanUp.py")
 #passing some variables to the pythonscript 
 #alert_id will be sys.argv[1]
@@ -29,6 +49,23 @@ class selfheal(Action):
 #day will be sys.argv[4]
 					if returnvalue != 0 :
 						return (False,"Error executing pythonfile on remote host")
+					output = subprocess.check_output("sudo ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'df -h\'", shell=True)
+					output = output.split("\n")
+					for x in range(1, len(output) - 1):
+					    if output[x].startswith("/dev/"):
+						disk = output[x].split(" ")
+						if "G" in disk[9] :
+						    diskused = disk[9].strip("G")
+						    diskused = diskused.replace(",", ".")
+						if "T" in disk[9] :
+						    diskused = disk[9].strip("T")
+						    diskused = diskused.replace(",", ".")
+						    diskused = float(diskused) * 1024
+						DiskSpaceAfterClean += float(diskused)
+					if DiskSpaceAfterClean =< (TotalDiskSpaceUsed - DiskSpaceToClean) :
+						print "genoeg schoongemaakt"
+					else :
+						print "nie genoeg schoongemaakt"
 					   			
 					returnvalue = os.system("ssh -i " + Pempath + " " + Username + "@" + Host + " \'sudo rm /tmp/"+alert_id+"\'")
 					if returnvalue != 0 :

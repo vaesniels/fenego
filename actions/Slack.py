4134ab1f32c0fe85c9a9804dadf55ace18f5773d
root <wtdniels@hotmail.com>
2017-05-10 14:57:55 +0200

    few changes to make slack work

--- actions/Slack.py
+++ actions/Slack.py
@@ -6,11 +6,14 @@ from st2actions.runners.pythonrunner import Action
 class Slack(Action):
     def run(self,message):
 	y = 0
+	msg = ""
 	MessageParts = message.split('\n')
 	print MessageParts
 	for x in MessageParts :
 		if MessageParts[y].startswith('Slack:') :
 		    msg = MessageParts[y].replace('Slack:', '')
 		y = y + 1
+	msg = msg.replace('"', '')
+	msg = msg.replace("'", "")
 	os.system('curl -X POST --data-urlencode \'payload={"channel": "#general", "username": "StackStorm", "text" :" ' + msg + '"}\' https://hooks.slack.com/services/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')
 

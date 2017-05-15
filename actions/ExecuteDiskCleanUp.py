98342d6ec2c6dd168899e8482dda935c9024e9d6
root <wtdniels@hotmail.com>
2017-05-15 11:22:32 +0200

    changes to disk clean

--- actions/ExecuteDiskCleanUp.py
+++ actions/ExecuteDiskCleanUp.py
@@ -52,6 +52,11 @@ class ExecuteDiskCleanUp(Action):
 	#day will be sys.argv[4]
 					if returnvalue != 0 :
 						return (False,"Error executing pythonfile on remote host")
+					returnvalue = os.system("ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host  + " \'sudo find /tmp/FileWasToBig\'"
+					if returncode == 0:
+    						os.system("ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host  + " \'sudo rm /tmp/FileWasToBig\'"
+						msg = "A file on Host " + host + " of Company " + company + " could not be zipped because there was to less disk space, manual action is needed."
+						os.system('curl -X POST --data-urlencode \'payload={"channel": "#general", "username": "StackStorm", "text" :" ' + msg + '"}\' https://hooks.slack.com/services/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')
 
 					output = subprocess.check_output("sudo ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'df -h\'", shell=True)
 					output = output.split("\n")
--- python/DiskCleanUp.py
+++ python/DiskCleanUp.py
@@ -5,18 +5,54 @@ import datetime as DT
 import smtplib
 
 today = DT.datetime.today()
-deletedate = today - DT.timedelta(days=int(sys.argv[3]))
+Available = 0.0
+diskavailable = 0.0
+deletedate = today - DT.timedelta(days=int(2))
 paths = sys.argv[1].split("$") #sys.args[2] are the paths on the system that can be cleaned up.
 
 for i in range(0, len(paths)):
-    output = subprocess.check_output("sudo find " + paths[i]+ " -type f -size +"+sys.argv[2]+"M -exec ls -lh --time-style=long-iso {} \; 2> /dev/null"" | awk '{ print $NF \": \" $5 \": \" $6}'", shell=True) #This is going to generate a list of files that exceeds the predefined file size. The file size is defined in sys.argv[3].
+    output = subprocess.check_output("sudo find " + paths[i] + " -type f -size +1000M -exec ls -lh --time-style=long-iso {} \; 2> /dev/null"" | awk '{ print $NF \": \" $5 \": \" $6}'", shell=True) #This is going to generate a list of files that exceeds the predefined file size. The file size is defined in sys.argv[3].
     output = output.split("\n")
-    
+
     for x in range(0, len(output) - 1): #with the paths that can be cleaned.
         values = output[x].split(": ")
         date = DT.datetime.strptime(values[2], "%Y-%m-%d")
         if date > deletedate: #script will check if the file is older then the defined date. When it is , it's allowed to be deleted.
-            returncode = os.system("sudo zip -j " + values[0] + ".zip " + values[0])
-            if returncode == 0: #Only when zipping the file is successfull the original file will be deleted.
-                os.system("sudo rm " + values[0])
+            command = subprocess.check_output("df -h",shell=True)
+            command = command.split("\n")
+
+            for y in range(1, len(command) - 1):
+                if command[y].startswith("/dev/"):
+                    disk = command[y].split(" ")
+                    while '' in disk:
+                        disk.remove('')
+
+                    if "M" in disk[3]:
+                        diskavailable = disk[3].strip("M")
+                        diskavailable = diskavailable.replace(",", ".")
+                        diskavailable = float(diskavailable) / 1024
+                    if "G" in disk[3]:
+                        diskavailable = disk[3].strip("G")
+                        diskavailable = diskavailable.replace(",", ".")
+                    if "T" in disk[3]:
+                        diskavailable = disk[3].strip("T")
+                        diskavailable = diskavailable.replace(",", ".")
+                        diskavailable = float(diskavailable) * 1024
+                    Available += float(diskavailable)
+            if "M" in values[1]:
+                filesize = values[1].strip("M")
+                filesize = filesize.replace(",", ".")
+                SizeOfFile = float(filesize) / 1024
+            if "G" in values[1]:
+                filesize = values[1].strip("G")
+                filesize = filesize.replace(",", ".")
+                SizeOfFile = float(filesize)
+            if "T" in values[1]:
+                filesize = values[1].strip("T")
+                filesize = filesize.replace(",", ".")
+                SizeOfFile = float(filesize) * 1024
+            if ((SizeOfFile/100) * 20) > Available:
+		os.system("sudo touch /tmp/FileWasToBig")
+            else:
+                returncode = os.system("sudo gzip -f " + values[0])
 

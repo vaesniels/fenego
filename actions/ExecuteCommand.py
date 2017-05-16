d5037fafbc360f4b477c880a46e9d6b73df412c7
root <wtdniels@hotmail.com>
2017-05-16 10:06:45 +0200

    small changes

--- actions/ExecuteCommand.py
+++ actions/ExecuteCommand.py
@@ -8,13 +8,11 @@ from st2actions.runners.pythonrunner import Action
 
 
 class ExecuteCommand(Action):
-    def run(self, host, company, cmd, stackstormpath, status=""):
+    def run(self, host, company, cmd, stackstormpath):
 
         hostname = "null"
-	started = "No"
         #try:  # trying to make an SSH connection and run a command
-        with open(
-                                        stackstormpath + "SSH/" + company + '_SSH') as ReadFile:  # Opens the SSH information file from the company.
+        with open(stackstormpath + "SSH/" + company + '_SSH') as ReadFile:  # Opens the SSH information file from the company.
             for line in ReadFile:  # Reads the file and loads the variables
                 Credentials = line.split("; ")
                 Credentials[0] = Credentials[0].strip("host:")
@@ -33,23 +31,7 @@ class ExecuteCommand(Action):
                         print "Slack:An error occured while trying to execute the command: \"" + cmd + "\" on the Host: " + host + " of Company: " + company + " Errorcode 256 : Command not found"
                         return (False, "Error executing command , Command not found")
                     if returnvalue == 0:
-                        if status != "":
-                            x = 0
-                            now = datetime.datetime.now()
-                            nu = now.strftime('%Y%m%d')
-                            status = status + "console-" + str(nu) + ".log"
-                            while x < 10:
-                                x = x + 1
-                                time.sleep(30)
-                                output = subprocess.check_output(
-                                    "sudo ssh -o StrictHostKeyChecking=No -i " + Pempath + " " + Username + "@" + Host + " \'tail -200 " + status + " \'",
-                                    shell=True)
-                                if "INFO: Server startup in" in output:
-                                    x = 10
-                                    started = "yes"
-                        if started != "yes":
-                            print "Slack:Executed the command : " + cmd + "\", on the Host: " + host + " of Company: " + company + " , But the service did nott start."
-                            return (False, "Executed the command, but the service did not start")
+                       return True
 
                     else:
                         print "Slack:An error occured while trying to execute the command: \"" + cmd + "\", on the Host: " + host + " of Company: " + company

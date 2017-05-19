b55ce0ad32ab2be39492fa75487e446677b36244
root <wtdniels@hotmail.com>
2017-05-19 11:23:58 +0200

    changed CreateJSON

--- python/CreateJSON.py
+++ python/CreateJSON.py
@@ -1,22 +1,16 @@
+#!/usr/bin/env python
+
 import os
 import subprocess
 from datetime import datetime, timedelta
 
-outputlist = ""
-output = subprocess.check_output("cd /home/niels/Jsonfiles/ && ls", shell=True)
-#print output
-output = output.replace(".json", "")
-#print output
-max = max(output)
-#print max
-index = int(max) + 1
 now = datetime.utcnow()
 now = now.strftime('%Y-%m-%dT%H:%M:%S.000000Z')
-hourago = datetime.utcnow() - timedelta(hours=1)
-hourago = hourago.strftime('%Y-%m-%dT%H:%M:%S.000000Z')
+SomeTimeAgo = datetime.utcnow() - timedelta(minutes=1)
+SomeTimeAgo = SomeTimeAgo.strftime('%Y-%m-%dT%H:%M:%S.000000Z')
 #print now
 #print hourago
-output = subprocess.check_output("sudo st2 execution list -tg '" + hourago +"' -tl '"+ now +"' -n 100", shell=True)
+output = subprocess.check_output("sudo st2 execution list -tg '" + SomeTimeAgo +"' -tl '"+ now +"' -n 100", shell=True)
 #print output
 outputlist = output
 outputlist = outputlist.split("\n")
@@ -33,11 +27,4 @@ for x in range(3, len(outputlist)-2):
             output = subprocess.check_output("sudo st2 execution get " + ExecutionID + " -j", shell=True)
             output = output.split("[\n")
             #print output
-            file = open("/home/niels/Jsonfiles/" + str(index) + ".json", "w")
-            file.write(output[0])
-            file.close()
-            os.system("curl -XPOST 'http://localhost:9200/pythontest/pythonjson/" + str(index) + "' -d " + "@/home/niels/Jsonfiles/" + str(index) + ".json")
-            index = index +1
-
-
-
+            os.system("curl -XPOST 'http://localhost:9200/pythontest/pythonjson/' -d \'" + output[0] + "\'")
